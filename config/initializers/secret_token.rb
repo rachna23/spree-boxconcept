cab2f8a72e2f4019e51fd45779654e02fe58e39b
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-11 15:17:42 +0530

    spree1.3 in rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoxConcept::Application.config.secret_token = '1deb8982d2eb90aa9a30e98819f7ab79cf40cc97279398e4c3fe4166467845ddb1b7be0da11207a95d94f27c7aef790665a667965dc2a9e6bc1be72512923a5c'
