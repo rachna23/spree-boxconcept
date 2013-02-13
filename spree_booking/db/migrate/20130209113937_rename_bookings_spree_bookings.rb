82bfa7ce54a9202e579e23e1cb77b3a36c9487d6
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-13 12:14:32 +0530

    commit for heroku

--- spree_booking/db/migrate/20130209113937_rename_bookings_spree_bookings.rb
+++
@@ -1,9 +0,0 @@
-class RenameBookingsSpreeBookings < ActiveRecord::Migration
-  def up
-  	rename_table :bookings, :spree_bookings
-  end
-
-  def down
-  	rename_table :bookings, :spree_bookings
-  end
-end
