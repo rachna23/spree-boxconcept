64a08d8c0751534584bf9bb5d3589fc6a9839828
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-19 20:37:47 +0530

    modify page translation db file

--- db/migrate/20130218144824_add_translation_to_spree_pages.rb
+++ db/migrate/20130218144824_add_translation_to_spree_pages.rb
@@ -1,5 +1,5 @@
 class AddTranslationToSpreePages < ActiveRecord::Migration
   def change
-  	Spree::Page.create_translation_table! :title => :string, :body => :text
+  	Spree::Page.create_translation_table!({:title => :string, :body => :text}, { :migrate_data => true } )
   end
 end
