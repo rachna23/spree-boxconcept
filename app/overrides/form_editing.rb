e26ca6b2b65f9c6902d58ec737b6f8d7fe1a9d83
Sibanand CIS <sibananda.k@cisinlabs.com>
2013-02-26 11:03:53 +0530

    add form tab

--- app/overrides/form_editing.rb
+++ app/overrides/form_editing.rb
@@ -3,4 +3,6 @@ Deface::Override.new(
   :name => 'add_showcase_to_admin_configurations_side_menu',
   :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
   :text => %q{<%= configurations_sidebar_menu_item t(:forms), admin_forms_path %>}
+
+  
 )
